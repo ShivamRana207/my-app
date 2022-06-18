0d052a024438cacb819d387d58aad2e2e2feef43
ranashivam207 <ranashivam207@gmail.com>
2022-06-18 20:34:57 +0530

    finish style of navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <ul className={styles.navbar}>
     <Link href='/'><li><a>Home</a></li></Link>
-    <Link href='/blog'><li><a>Blog</a></li></Link>
+    <Link href='/blogs'><li><a>Blog</a></li></Link>
     <Link href='/services'><li><a>Services</a></li></Link>
     <Link href='/about'><li><a>About</a></li></Link>
 </ul>
