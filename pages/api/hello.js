fdde28bdf32a36403ed4ca968e3b5a47d2ba55ea
ranashivam207 <ranashivam207@gmail.com>
2022-06-20 13:42:20 +0530

    creatin api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from 'fs';
+
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Shivam' })
+  fs.readFile('blogdata/blog1.json' ,'utf-8', (err, data)=>{
+    console.log();
+    res.status(200).json(JSON.parse(data));
+  })
+  
 }
