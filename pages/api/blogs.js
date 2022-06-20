0617d2ca586ec3c3b2a23bfad3a92c1903c0dcfb
ranashivam207 <ranashivam207@gmail.com>
2022-06-20 14:25:56 +0530

    creating api end point and getting all blogs

---
+++ pages/api/blogs.js
@@ -0,0 +1,10 @@
+import * as fs from 'fs';
+
+
+export default function handler(req, res) {
+  fs.readdir('blogdata' , (err, data)=>{
+    console.log(data);
+    res.status(200).json(data);
+  })
+  
+}
--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -3,9 +3,12 @@ import * as fs from 'fs';
 
 
 export default function handler(req, res) {
-  fs.readFile('blogdata/blog1.json' ,'utf-8', (err, data)=>{
-    console.log();
+  fs.readFile(`blogdata/${req.query.slug}.json`,'utf-8', (err, data) => {
+    // if (err) {
+    //   res.status(500).json({ Error: 'No result found' })
+    // }
+    console.log(req.query.slug);
     res.status(200).json(JSON.parse(data));
   })
-  
+
 }
